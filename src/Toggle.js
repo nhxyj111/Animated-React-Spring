cdd1b24b085e7bc2192b7f514c26d42de5758111
YeJ XU <nhxyj111@gmail.com>
2019-05-20 10:19:29 +0800

    10

--- src/Toggle.js
+++ src/Toggle.js
@@ -8,7 +8,7 @@ const Toggle = () => {
     color: isToggle ? 'tomato' : 'green',
     // fontSize: isToggle ? '2em' : '8em',
     // transform: isToggle ? 'translate3d(0,0,0)' : 'translate3d(0,-50px,0)',
-    y: isToggle ? 0 : -50,
+    y: isToggle ? 0 : 1,
   })
 
   return (
@@ -16,7 +16,12 @@ const Toggle = () => {
       <animated.h1
         style={{
           ...fade,
-          transform: fade.y.interpolate(y => `translate3d(0, ${y}px, 0)`),
+          transform: fade.y
+            .interpolate({
+              range: [0, 0.25, 0.5, 0.75, 1],
+              output: [0, -25, -50, -100, -50],
+            })
+            .interpolate(y => `translate3d(0, ${y}px, 0)`),
         }}
       >
         Hello
