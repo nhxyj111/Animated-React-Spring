620cda6ad6d7fdb162f9bcd4b3c96cd66243a3cc
YeJ XU <nhxyj111@gmail.com>
2019-05-20 10:32:28 +0800

    11

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,31 +1,44 @@
 import React, { useState } from 'react'
-import { useSpring, animated } from 'react-spring'
+import { useTransition, animated } from 'react-spring'
 
 const Toggle = () => {
   const [isToggle, setToggle] = useState(false)
-  const fade = useSpring({
-    // opacity: isToggle ? 1 : 0,
-    color: isToggle ? 'tomato' : 'green',
-    // fontSize: isToggle ? '2em' : '8em',
-    // transform: isToggle ? 'translate3d(0,0,0)' : 'translate3d(0,-50px,0)',
-    y: isToggle ? 0 : 1,
+  // const fade = useSpring({
+  //   // opacity: isToggle ? 1 : 0,
+  //   color: isToggle ? 'tomato' : 'green',
+  //   // fontSize: isToggle ? '2em' : '8em',
+  //   // transform: isToggle ? 'translate3d(0,0,0)' : 'translate3d(0,-50px,0)',
+  //   y: isToggle ? 0 : 1,
+  // })
+  const transition = useTransition(isToggle, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
   })
 
   return (
     <div>
-      <animated.h1
-        style={{
-          ...fade,
-          transform: fade.y
-            .interpolate({
-              range: [0, 0.25, 0.5, 0.75, 1],
-              output: [0, -25, -50, -100, -50],
-            })
-            .interpolate(y => `translate3d(0, ${y}px, 0)`),
-        }}
+      {transition.map(
+        ({ item, key, props }) =>
+          item && (
+            <animated.h1 key={key} style={props}>
+              Hello
+            </animated.h1>
+          )
+      )}
+      {/* <animated.h1
+      // style={{
+      //   ...fade,
+      //   transform: fade.y
+      //     .interpolate({
+      //       range: [0, 0.25, 0.5, 0.75, 1],
+      //       output: [0, -25, -50, -100, -50],
+      //     })
+      //     .interpolate(y => `translate3d(0, ${y}px, 0)`),
+      // }}
       >
         Hello
-      </animated.h1>
+      </animated.h1> */}
       <button onClick={() => setToggle(!isToggle)}>Toggle</button>
     </div>
   )
